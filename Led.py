cb375a652399c73fe91ba60d0b971e650f6d262f
sathishkumar-24 <109523140+sathishkumar-24@users.noreply.github.com>
2022-09-14 12:52:35 +0530

    Update Led.py

--- Led.py
+++ Led.py
@@ -1,16 +1,2 @@
 import time
 import RPi.GPIO as GPIO
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-pinout = 18
-color = "Green"
-
-GPIO.setup(pinout,GPIO.OUT)
-
-GPIO.output(pinout,GPIO.HIGH)
-time.sleep(1)
-
-GPIO.output(pinout,GPIO.LOW)
-time.sleep(1)
