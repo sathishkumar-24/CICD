53edbb21e12af78f81a6dcb128f55d8a828b4a2a
sathishkumar-24 <109523140+sathishkumar-24@users.noreply.github.com>
2022-09-14 14:46:04 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
-#include<stdio.h>
-int main(){
-   char a[20] = "Names", b[20]="amount to be paid";
-   char a1[20] = "Bhanu", b1[20]="Hari",c1[20]="Lucky",d1[20]="Puppy";
-   int a2=200,b2=400,c2=250,d2=460;
-   printf("%-15s %-15s\n", a, b);
-   printf("%-15s %-15d\n", a1,a2);
-   printf("%-15s %-15d\n", b1,b2);
-   printf("%-15s %-15d\n", c1, c2);
-   printf("%-15s %-15d\n", d1, dr);
-   return 0;
+#include <stdio.h>
+int main() {    
+
+    int number1, number2, sum;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    // calculating sum
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0;
 }
