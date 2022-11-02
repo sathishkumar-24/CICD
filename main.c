f183e484a2af6800cdadd3d84d18b7b6de836056
sathishkumar-24 <109523140+sathishkumar-24@users.noreply.github.com>
2022-11-02 12:27:30 +0530

    hello world

--- main.c
+++ main.c
@@ -7,12 +7,10 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 
 #include <stdio.h>
-int main() {    
 
-    int number1, number2, sum;
+int main()
+{
+    printf("Hello World");
 
-    sum = number1 + number2;      
-    
-    printf("%d + %d = %d", number1, number2, sum);
     return 0;
 }
