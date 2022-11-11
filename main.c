9eb9beb3327dc672081ee540350a351d058e75ac
sathishkumar-24 <109523140+sathishkumar-24@users.noreply.github.com>
2022-11-11 14:57:49 +0530

    Update main.c

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main() {
     int number1, number2, sum;
 
     sum = number1 + number2;
-  sum = number1 + number2; 
+    sum = number1 + number2; 
     
     printf("%d + %d = %d", number1, number2, sum);
     return 0;
