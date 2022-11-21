4bc7cf42fbc1063460207bee42c83d025139c189
sathishkumar-24 <109523140+sathishkumar-24@users.noreply.github.com>
2022-11-21 15:06:46 +0530

    Update main.c

--- main.c
+++ main.c
@@ -7,13 +7,11 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 
 #include <stdio.h>
-int main()  {    
-
-    int number1, number2, sum;
-int main()
+int main() 
 {
-    printf("Hello World");
-      sum = number1 + number2;
- printf("%d + %d = %d", number1, number2, sum);
-    return 0;
+  int number1, number2, sum;
+  printf("Hello World");
+  sum = number1 + number2;
+  printf("%d + %d = %d", number1, number2, sum);
+  return 0;
 }
