042a04a8c87993f4eb7455ee6d51e979d68ccc4b
sathishkumar-24 <109523140+sathishkumar-24@users.noreply.github.com>
2022-11-21 15:29:42 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1883 Thomas Edison - All Rights Reserved
+/* Copyright (C) 2022 sathish Edison - All Rights Reserved
  * You may use, distribute and modify this code under the
  * terms of the XYZ license, which unfortunately won't be
  * written for another century.
@@ -6,13 +6,6 @@
  * You should have received a copy of the XYZ license with
  * this file. If not, please write to: , or visit :
  */
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 #include <stdio.h>
 
