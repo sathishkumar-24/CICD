9179c2153ee841320737afbdba8e9a02ba51cdce
sathishkumar-24 <109523140+sathishkumar-24@users.noreply.github.com>
2023-03-22 14:48:56 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,20 +4,14 @@
 int main()
 {
   int A, B, sum = 0;
-  char *foo1;
-  char &foo2;
-  string ^s1;
+
   // Ask user to enter the two numbers
   printf("Enter two numbers A and B : \n");
   // Read two numbers from the user || A = 2, B = 3
   scanf("%d%d", &A, &B);
   // Calculate the addition of A and B
-  // using '+' operator
-  sum = A + B; // using '+' operator
-  sum = A + B;
 
   for (int i = 0; i <= 10; i++) {
-    sum = A + B;
     sum = A + B;
   }
 
