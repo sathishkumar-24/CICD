01391aaae7bca18caebd6ebcd7b370dd24635581
sathishkumar-24 <109523140+sathishkumar-24@users.noreply.github.com>
2023-03-22 14:43:36 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,27 @@
-/* Copyright (C) 2022 sathish Edison - All Rights Reserved
- * You may use, distribute and modify this code under the
- * terms of the XYZ license, which unfortunately won't be
- * written for another century.
- *
- * You should have received a copy of the XYZ license with
- * this file. If not, please write to: , or visit :
- */
-
+// C program to add two numbers
 #include <stdio.h>
 
 int main()
 {
-    int A, B, sum = 0;
-    printf("Hello World");
-  printf("Hello World");
-    // Calculate the addition of A and B
-    // using '+' operator
-    sum = A +B;
-  
-    // Print the sum
-    printf("Sum of A and B is: %d", sum);
-    
-    return 0;
+  int A, B, sum = 0;
+  char *foo1;
+  char &foo2;
+  string ^s1;
+  // Ask user to enter the two numbers
+  printf("Enter two numbers A and B : \n");
+  // Read two numbers from the user || A = 2, B = 3
+  scanf("%d%d", &A, &B);
+  // Calculate the addition of A and B
+  // using '+' operator
+  sum = A + B; // using '+' operator
+  sum = A + B;
+
+  for (int i = 0; i <= 10; i++) {
+    sum = A + B;
+    sum = A + B;
+  }
+
+  // Print the sum
+  printf("Sum of A and B is: %d", sum);
+  return 0;
 }
