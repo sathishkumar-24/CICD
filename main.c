b844ebe70c575a8c4d188c549e630abc71f95fd5
sathishkumar-24 <109523140+sathishkumar-24@users.noreply.github.com>
2023-09-29 19:12:16 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 int main()
 {
-  // printf() displays the string inside quotation t
-  printf("Hello, World!");
+  // printf() displays the string inside quotation
   printf("Hello, World!");
   return 0;
 }
